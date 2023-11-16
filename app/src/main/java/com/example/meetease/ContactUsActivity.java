66ca5161854c768dad34ce9fc8c8451635e6fcce
AzizMKachwala <akachwala24@gmail.com>
2023-11-16 18:42:39 +0530

    day16

---
+++ app/src/main/java/com/example/meetease/ContactUsActivity.java
@@ -0,0 +1,27 @@
+package com.example.meetease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    ImageView ivBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+
+        ivBack = findViewById(R.id.ivBack);
+
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
