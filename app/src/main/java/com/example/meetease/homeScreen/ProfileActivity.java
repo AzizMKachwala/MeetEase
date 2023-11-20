1b28cd61d2beb9eb48f946c526f582629b3986e7
AimsProject <anand.patel@yudiz.com>
2023-11-20 18:53:01 +0530

    day-20 last

--- app/src/main/java/com/example/meetease/homeScreen/ProfileActivity.java
+++ app/src/main/java/com/example/meetease/homeScreen/ProfileActivity.java
@@ -24,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.meetease.R;
 import com.example.meetease.appUtils.PreferenceManager;
+import com.example.meetease.appUtils.Tools;
 import com.example.meetease.appUtils.VariableBag;
 import com.example.meetease.network.RestCall;
 import com.example.meetease.network.RestClient;
@@ -45,6 +46,7 @@ public class ProfileActivity extends AppCompatActivity {
 
     ImageView ivBack, imgEdit;
     CircleImageView imgProfileImage;
+    Tools tools;
     EditText etvFullName, etvPhoneNo, etvEmail;
     Button btnSave;
     String currentPhotoPath = "";
@@ -57,12 +59,15 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
-
+        tools = new Tools(this);
         restCall = RestClient.createService(RestCall.class, VariableBag.BASE_URL, VariableBag.API_KEY);
 
         ivBack = findViewById(R.id.ivBack);
         imgEdit = findViewById(R.id.imgEdit);
+        btnSave = findViewById(R.id.btnSave);
+        etvFullName = findViewById(R.id.etvFullName);
+        etvPhoneNo = findViewById(R.id.etvPhoneNo);
+        etvEmail = findViewById(R.id.etvEmail);
         imgProfileImage = findViewById(R.id.imgProfileImage);
 
         ivBack.setOnClickListener(new View.OnClickListener() {
@@ -72,6 +77,33 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etvFullName.getText().toString().isEmpty()) {
+                    etvFullName.setError("Enter Name");
+                    etvFullName.requestFocus();
+                } else if (etvFullName.getText().toString().length() < 2) {
+                    etvFullName.setError("Enter Valid Name");
+                    etvFullName.requestFocus();
+                }
+                else if (etvPhoneNo.getText().toString().isEmpty()) {
+                    etvPhoneNo.setError("Enter Mobile Number");
+                    etvPhoneNo.requestFocus();
+                } else if (etvPhoneNo.length() != 10) {
+                    etvPhoneNo.setError("Enter Mobile Number with 10 Digits");
+                    etvPhoneNo.requestFocus();
+                } else if (etvEmail.getText().toString().isEmpty()) {
+                    etvEmail.setError("Email Address cannot be Empty");
+                    etvEmail.requestFocus();
+                } else if (!Tools.isValidEmail(etvEmail.getText().toString())) {
+                    etvEmail.setError("Email Address must contain @ and .com in it");
+                    etvEmail.requestFocus();
+                }else {
+                    editUser();
+                }
+            }
+        });
         imgEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -146,15 +178,20 @@ public class ProfileActivity extends AppCompatActivity {
         return image;
     }
     void editUser(){
-        PreferenceManager preferenceManager = new PreferenceManager(this);
-        String user_id = preferenceManager.getKeyValueString(VariableBag.user_id,"");
 
-        RequestBody tag = RequestBody.create(MediaType.parse("text/plain"),"UpdateUser");
-        RequestBody user_id = RequestBody.create(MediaType.parse("text/plain"), categoryId);
-        RequestBody full_name = RequestBody.create(MediaType.parse("text/plain"), categoryId);
-        RequestBody mobile = RequestBody.create(MediaType.parse("text/plain"), subCategoryId);
-        RequestBody email = RequestBody.create(MediaType.parse("text/plain"), etvProductName.getText().toString());
-        RequestBody password = RequestBody.create(MediaType.parse("text/plain"), etvProductPrice.getText().toString());
+        PreferenceManager preferenceManager = new PreferenceManager(this);
+        String user_id_temp = preferenceManager.getKeyValueString(VariableBag.user_id,"");
+        String full_name_temp = preferenceManager.getKeyValueString(VariableBag.full_name,"");
+        String mobile_temp = preferenceManager.getKeyValueString(VariableBag.mobile,"");
+        String email_temp = preferenceManager.getKeyValueString(VariableBag.email,"");
+        String password_temp = preferenceManager.getKeyValueString(VariableBag.password,"");
+
+        RequestBody  tag = RequestBody.create(MediaType.parse("text/plain"),"UpdateUser");
+        RequestBody user_id = RequestBody.create(MediaType.parse("text/plain"), user_id_temp);
+        RequestBody full_name = RequestBody.create(MediaType.parse("text/plain"), full_name_temp);
+        RequestBody mobile = RequestBody.create(MediaType.parse("text/plain"), mobile_temp);
+        RequestBody email = RequestBody.create(MediaType.parse("text/plain"), email_temp);
+        RequestBody password = RequestBody.create(MediaType.parse("text/plain"), password_temp);
         MultipartBody.Part fileToUploadfile = null;
         if (fileToUploadfile == null && currentPhotoPath != "") {
             try {
@@ -180,11 +217,29 @@ public class ProfileActivity extends AppCompatActivity {
                     @Override
                     public void onError(Throwable e) {
 
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(ProfileActivity.this, "no internet connection", Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }
 
                     @Override
                     public void onNext(UserResponse userResponse) {
-
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                preferenceManager.setKeyValueBoolean(VariableBag.SessionManage,true);
+                                preferenceManager.setKeyValueString(VariableBag.full_name,etvFullName.getText().toString());
+                                preferenceManager.setKeyValueString(VariableBag.mobile,etvPhoneNo.getText().toString());
+                                preferenceManager.setKeyValueString(VariableBag.email,etvEmail.getText().toString());
+                                Toast.makeText(ProfileActivity.this, userResponse.getMessage(), Toast.LENGTH_SHORT).show();
+                                if (userResponse.getStatus().equals(VariableBag.SUCCESS_RESULT)){
+                                    finish();
+                                }
+                            }
+                        });
                     }
                 });
 
