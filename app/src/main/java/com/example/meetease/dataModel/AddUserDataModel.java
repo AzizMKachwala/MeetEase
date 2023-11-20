429a564bcb440e43dad4d7da2e78c1cbdc772cea
AimsProject <anand.patel@yudiz.com>
2023-11-20 10:43:57 +0530

    day-20

---
+++ app/src/main/java/com/example/meetease/dataModel/AddUserDataModel.java
@@ -0,0 +1,4 @@
+package com.example.meetease.dataModel;
+
+public class AddUserDataModel {
+}
--- app/src/main/java/com/example/meetease/network/RestCall.java
+++ app/src/main/java/com/example/meetease/network/RestCall.java
@@ -1,13 +1,20 @@
 package com.example.meetease.network;
 
 
+import com.example.meetease.dataModel.AddUserDataModel;
+
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+import rx.Single;
+
 public interface RestCall {
-//    @FormUrlEncoded
-//    @POST("CategoryController.php")
-//    Single<CommonResponse> AddCategory(
-//            @Field("tag") String tag,
-//            @Field("user_id") String user_id,
-//            @Field("category_name") String category_name);
+    @FormUrlEncoded
+    @POST("CategoryController.php")
+    Single<AddUserDataModel> AddCategory(
+            @Field("tag") String tag,
+            @Field("user_id") String user_id,
+            @Field("category_name") String category_name);
 
 
 }
